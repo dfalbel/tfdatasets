551a52b728f9bce649c1502f76081638cfaa5fe9
Daniel Falbel <dfalbel@gmail.com>
2019-04-16 16:12:33 -0300

    an example of how it would look like

--- examples/eager/tfdatasets_eager.R
+++ examples/eager/tfdatasets_eager.R
@@ -1,8 +1,9 @@
 
-library(tensorflow)
-tfe_enable_eager_execution()
+# library(tensorflow)
+#tfe_enable_eager_execution()
 
 library(tfdatasets)
+#library(flowery)
 
 record_spec <- sql_record_spec(
   names = c("disp", "drat", "vs", "gear", "mpg", "qsec", "hp", "am", "wt","carb", "cyl"),
@@ -10,14 +11,14 @@ record_spec <- sql_record_spec(
             tf$float64, tf$int32, tf$int32, tf$int32, tf$int32)
 )
 
-dataset <- sqlite_dataset('mtcars.sqlite3', 'select * from mtcars', record_spec) %>%
+dataset <- sqlite_dataset('examples/eager/mtcars.sqlite3', 'select * from mtcars', record_spec) %>%
   dataset_batch(10)
 
-iter <- make_iterator_one_shot(dataset)
 
-until_out_of_range({
-  batch <- iterator_get_next(iter)
-  str(batch)
+iter <- make_dataset_iterator(dataset)
+
+flowery::iterate(for(x in iter) {
+  print(str(x))
 })
 
 
